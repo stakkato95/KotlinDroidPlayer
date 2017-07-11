288a3dcdba5393aeddc34fe0b5d7d8e6b161c245
Artsiom Kaliaha <stakkato95@gmail.com>
2017-07-11 14:13:11 +0300

    work in progress

--- KMusic/app/src/main/java/com/github/stakkato95/kmusic/MainActivity.kt
+++ KMusic/app/src/main/java/com/github/stakkato95/kmusic/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
+import android.widget.ScrollView
 import com.github.stakkato95.kmusic.common.extensions.blur
 import com.github.stakkato95.kmusic.player.PlayerPagerAdapter
 
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val pager = findViewById(R.id.pager) as ViewPager
         with(pager) {
             adapter = PlayerPagerAdapter(supportFragmentManager)
@@ -21,10 +23,18 @@ class MainActivity : AppCompatActivity() {
             setPadding(leftRightPadding, 0, leftRightPadding, 0)
         }
 
+
         val cover = findViewById(R.id.cover_image) as ImageView
         val blurredImage = (cover.drawable as BitmapDrawable).bitmap.blur(this, 0.5f, 12.5f)
         cover.setImageBitmap(blurredImage)
 
-        findViewById(R.id.album_text).isSelected = true
+
+        val albumText = findViewById(R.id.album_text)
+        val scroll = findViewById(R.id.scroll) as ScrollView
+        val container = findViewById(R.id.container)
+
+        scroll.viewTreeObserver.addOnScrollChangedListener {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
     }
 }
