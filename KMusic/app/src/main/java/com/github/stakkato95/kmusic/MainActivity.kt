599041a745fec3cc4088cd3294bb812b19df31f1
Artsiom Kaliaha <stakkato95@gmail.com>
2017-07-11 13:29:26 +0300

    added track info screen

--- KMusic/app/src/main/java/com/github/stakkato95/kmusic/MainActivity.kt
+++ KMusic/app/src/main/java/com/github/stakkato95/kmusic/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.github.stakkato95.kmusic
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import com.github.stakkato95.kmusic.common.extensions.blur
 import com.github.stakkato95.kmusic.player.PlayerPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_test)
+        setContentView(R.layout.activity_main)
 
         val pager = findViewById(R.id.pager) as ViewPager
         with(pager) {
@@ -17,5 +20,11 @@ class MainActivity : AppCompatActivity() {
             val leftRightPadding = resources.displayMetrics.widthPixels / 6
             setPadding(leftRightPadding, 0, leftRightPadding, 0)
         }
+
+        val cover = findViewById(R.id.cover_image) as ImageView
+        val blurredImage = (cover.drawable as BitmapDrawable).bitmap.blur(this, 0.5f, 12.5f)
+        cover.setImageBitmap(blurredImage)
+
+        findViewById(R.id.album_text).isSelected = true
     }
 }
