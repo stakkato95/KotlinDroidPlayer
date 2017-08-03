222c96e8c40da4971225ed3b7a49af38da971b92
Artsiom Kaliaha <stakkato95@gmail.com>
2017-08-03 18:16:41 +0300

    added outline of tracks list

---
+++ KMusic/app/src/main/java/com/github/stakkato95/kmusic/tracks/Track.kt
@@ -0,0 +1,6 @@
+package com.github.stakkato95.kmusic.tracks
+
+/**
+ * Created by artsiomkaliaha on 03.08.17.
+ */
+data class Track(private val name: String, private val author: String)
--- KMusic/app/src/main/java/com/github/stakkato95/kmusic/tracks/TracksFragment.kt
+++ KMusic/app/src/main/java/com/github/stakkato95/kmusic/tracks/TracksFragment.kt
@@ -2,17 +2,34 @@ package com.github.stakkato95.kmusic.tracks
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.stakkato95.kmusic.R
+import com.github.stakkato95.kmusic.tracks.adapter.TracksAdapter
+import kotlinx.android.synthetic.main.fragment_tracks.view.*
 
 /**
  * Created by artsiomkaliaha on 14.07.17.
  */
 class TracksFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_tracks, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_tracks, container, false)
+        with(view.tracksRecyclerView) {
+            adapter = TracksAdapter(getTracks())
+            layoutManager = GridLayoutManager(activity, 2)
+        }
+        return view
+    }
+
+    fun getTracks(): List<Track> {
+        val tracks = arrayListOf<Track>()
+        for (i in 0..20) {
+            tracks.add(Track("Need for Speed", "Lil John"))
+        }
+
+        return tracks
     }
 }
