b55613b5c3bdf3ceb9a2070fbd6094b7a1123676
Artsiom Kaliaha <stakkato95@gmail.com>
2017-09-11 15:55:06 +0300

    added base implementation of music playback

--- KMusic/app/src/main/java/com/github/stakkato95/kmusic/MainActivity.kt
+++ KMusic/app/src/main/java/com/github/stakkato95/kmusic/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.github.stakkato95.kmusic
 
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.github.stakkato95.kmusic.common.adapter.RootPagerAdapter
 import com.github.stakkato95.kmusic.tracks.view.ViewPagerCoordinator
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_player_button.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var player: ExoPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +45,8 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Tracks count = ${cursor.count}", Toast.LENGTH_LONG).show()
 
         cursor.close()
+
+        testExoPlayer()
     }
 
     fun setProgressBarTouchListener() {
@@ -41,5 +55,21 @@ class MainActivity : AppCompatActivity() {
                 verticalPagerView.canInterceptTouchEvents = !isScrollInProgress
             }
         }
+    }
+
+    fun testExoPlayer() {
+        player = ExoPlayerFactory.newSimpleInstance(DefaultRenderersFactory(this), DefaultTrackSelector(), DefaultLoadControl())
+        player.playWhenReady = true
+        initializePlayer()
+    }
+
+    fun initializePlayer() {
+        val uri = Uri.parse("/storage/emulated/0/Music/Wilkinson - Brand New.mp3")
+        val mediaSource = buildMediaSource(uri)
+        player.prepare(mediaSource)
+    }
+
+    fun buildMediaSource(uri: Uri): MediaSource {
+        return ExtractorMediaSource(uri, DefaultDataSourceFactory(this, "ua"), DefaultExtractorsFactory(), null, null)
     }
 }
---
+++ KMusic/app/src/main/java/com/github/stakkato95/kmusic/manager/PermissionManager.kt
@@ -0,0 +1,9 @@
+package com.github.stakkato95.kmusic.manager
+
+/**
+ * Created by artsiomkaliaha on 11.09.17.
+ */
+object PermissionManager {
+
+
+}
