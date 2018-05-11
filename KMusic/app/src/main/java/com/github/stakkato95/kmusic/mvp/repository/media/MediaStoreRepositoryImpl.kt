40f1aab5d09954a524efba2a1f2c8de0f474f664
Artsiom Kaliaha <stakkato95@gmail.com>
2018-05-11 14:48:48 +0200

    bitrate information left for the second release

--- KMusic/app/src/main/java/com/github/stakkato95/kmusic/mvp/repository/media/MediaStoreRepositoryImpl.kt
+++ KMusic/app/src/main/java/com/github/stakkato95/kmusic/mvp/repository/media/MediaStoreRepositoryImpl.kt
@@ -1,8 +1,6 @@
 package com.github.stakkato95.kmusic.mvp.repository.media
 
 import android.content.Context
-import android.media.MediaExtractor
-import android.media.MediaFormat
 import com.github.stakkato95.kmusic.mvp.repository.media.extractor.AlbumsExtractor
 import com.github.stakkato95.kmusic.mvp.repository.media.extractor.TrackExtractor
 import com.github.stakkato95.kmusic.mvp.repository.model.PlayerTrack
@@ -20,18 +18,19 @@ class MediaStoreRepositoryImpl(val context: Context) : MediaStoreRepository {
             val tracks = mutableListOf<PlayerTrack>()
             extractors.forEach { it.extract(context, tracks) }
 
-            tracks.forEach {
-                val extractor = MediaExtractor()
-                extractor.setDataSource(it.path)
-                val format = extractor.getTrackFormat(0)
-                try {
-                    it.bitRate = format.getInteger(MediaFormat.KEY_BIT_RATE) / 1000
-                    it.sampleRate = format.getInteger(MediaFormat.KEY_SAMPLE_RATE) / 1000
-                } catch (e: Exception) {
-                    //ignored
-                }
-                extractor.release()
-            }
+            //TODO for 2nd release
+//            tracks.forEach {
+//                val extractor = MediaExtractor()
+//                extractor.setDataSource(it.path)
+//                val format = extractor.getTrackFormat(0)
+//                try {
+//                    it.bitRate = format.getInteger(MediaFormat.KEY_BIT_RATE) / 1000
+//                    it.sampleRate = format.getInteger(MediaFormat.KEY_SAMPLE_RATE) / 1000
+//                } catch (e: Exception) {
+//                    //ignored
+//                }
+//                extractor.release()
+//            }
 
             it.onNext(tracks)
             it.onComplete()
